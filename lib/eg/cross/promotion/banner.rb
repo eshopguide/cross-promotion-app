6b81a7fdc49503eb2a8f6a106d8aeeebd38dcaa7
MariaEshopGuide <maria@eshop-guide.de>
2022-11-14 15:57:55 +0100

    Init gem project

---
+++ lib/eg/cross/promotion/banner.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "banner/version"
+
+module Eg
+  module Cross
+    module Promotion
+      module Banner
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
